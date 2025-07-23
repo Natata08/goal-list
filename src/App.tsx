e5a681a3c9b219c87477c5eaa666b371df7ba617
Natalia Lapina <lapinann08@gmail.com>
2025-07-23 13:49:11 +0200

    feat: implement dynamic goal list with add functionality using useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
 import Goal from './components/Goal.tsx'
 import goalsImg from './assets/goals.jpg'
 import Header from './components/Header.tsx'
+import { useState } from 'react'
+
+type Goal = {
+  title: string
+  description: string
+  id: number
+}
 
 export default function App() {
+  const [goals, setGoals] = useState<Goal[]>([])
+
+  function handleAddGoal() {
+    setGoals((prevGoals) => {
+      const newGoal: Goal = {
+        id: Math.random(),
+        title: 'Learn something new',
+        description: 'Learn it in depth',
+      }
+      return [...prevGoals, newGoal]
+    })
+  }
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1>Your Goals</h1>
       </Header>
-      <Goal title='Create a new app'>
-        <p>Create a new project using React and TS</p>
-      </Goal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <ul>
+        {goals.map(({ title, description, id }) => {
+          return (
+            <li key={id}>
+              <Goal title={title}>
+                <p>{description}</p>
+              </Goal>
+            </li>
+          )
+        })}
+      </ul>
     </main>
   )
 }
