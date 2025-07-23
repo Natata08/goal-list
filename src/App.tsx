0a235b2ece2a7f28cda53fa2fedb540079155d6a
Natalia Lapina <lapinann08@gmail.com>
2025-07-23 12:35:19 +0200

    feat: add Header component with image and title support

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import Goal from './components/Goal.tsx'
+import goalsImg from './assets/goals.jpg'
+import Header from './components/Header.tsx'
 
 export default function App() {
   return (
     <main>
+      <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
+        <h1>Your Goals</h1>
+      </Header>
       <Goal title='Create a new app'>
         <p>Create a new project using React and TS</p>
       </Goal>
