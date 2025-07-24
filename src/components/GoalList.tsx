c922c68f6fc51bc5b8f094f49a3969d84cfcd2e6
Natalia Lapina <lapinann08@gmail.com>
2025-07-24 14:06:50 +0200

    feat: implement tiered warning system for goal count with severity levels

--- src/components/GoalList.tsx
+++ src/components/GoalList.tsx
@@ -3,7 +3,11 @@ import type { ReactNode } from 'react'
 import Goal from './Goal.tsx'
 import InfoBox from './InfoBox.tsx'
 
-const MAX_GOALS = 4
+const GOAL_THRESHOLDS = {
+  LOW_WARNING: 3,
+  MEDIUM_WARNING: 5,
+  HIGH_WARNING: 8
+}
 
 type GoalListProps = {
   goals: GoalType[]
@@ -16,12 +20,26 @@ export default function GoalList({ goals, onDeleteGoal }: GoalListProps) {
       <InfoBox mode='hint'>You have no goals yet. Start adding some!</InfoBox>
     )
   }
-  let warningBox: ReactNode
 
-  if (goals.length >= MAX_GOALS) {
+  let warningBox: ReactNode
+  const goalCount = goals.length
+
+  if (goalCount >= GOAL_THRESHOLDS.HIGH_WARNING) {
     warningBox = (
-      <InfoBox mode='warning'>
-        You are collecting a lot of goals. Don't put too much on your plate.
+      <InfoBox mode='warning' severity='high'>
+        🚨 You have {goalCount} goals! This might be overwhelming. Consider focusing on your top priorities first.
+      </InfoBox>
+    )
+  } else if (goalCount >= GOAL_THRESHOLDS.MEDIUM_WARNING) {
+    warningBox = (
+      <InfoBox mode='warning' severity='medium'>
+        ⚠️ You're managing {goalCount} goals. Make sure you can realistically achieve them all.
+      </InfoBox>
+    )
+  } else if (goalCount >= GOAL_THRESHOLDS.LOW_WARNING) {
+    warningBox = (
+      <InfoBox mode='warning' severity='low'>
+        💡 You have {goalCount} goals. Great start! Remember to stay focused on what matters most.
       </InfoBox>
     )
   }
