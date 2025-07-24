56ec036da2e57d63b1d5c348a7c6f2f5d891b5d6
Natalia Lapina <lapinann08@gmail.com>
2025-07-24 12:18:25 +0200

    feat: add InfoBox component with warning and hint states for goal list management

--- src/components/GoalList.tsx
+++ src/components/GoalList.tsx
@@ -1,5 +1,9 @@
-import Goal from './Goal.tsx'
 import type { Goal as GoalType } from '../App.tsx'
+import type { ReactNode } from 'react'
+import Goal from './Goal.tsx'
+import InfoBox from './InfoBox.tsx'
+
+const MAX_GOALS = 4
 
 type GoalListProps = {
   goals: GoalType[]
@@ -7,17 +11,35 @@ type GoalListProps = {
 }
 
 export default function GoalList({ goals, onDeleteGoal }: GoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode='hint'>You have no goals yet. Start adding some!</InfoBox>
+    )
+  }
+  let warningBox: ReactNode
+
+  if (goals.length >= MAX_GOALS) {
+    warningBox = (
+      <InfoBox mode='warning'>
+        You are collecting a lot of goals. Don't put too much on your plate.
+      </InfoBox>
+    )
+  }
+
   return (
-    <ul>
-      {goals.map(({ title, description, id }) => {
-        return (
-          <li key={id}>
-            <Goal id={id} title={title} onDelete={onDeleteGoal}>
-              <p>{description}</p>
-            </Goal>
-          </li>
-        )
-      })}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map(({ title, description, id }) => {
+          return (
+            <li key={id}>
+              <Goal id={id} title={title} onDelete={onDeleteGoal}>
+                <p>{description}</p>
+              </Goal>
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
